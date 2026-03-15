db789bd6c614d06b7237de081fefb67ad86632c5
Costa Robe <56robik@gmail.com>
2026-03-15 13:11:38 +0100

    ok

--- src/SilniaWhile.java
+++ src/SilniaWhile.java
@@ -1,13 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.Scanner;
+/**
+ * 2. Wzorując się na przykładzie 1 i znając algorytm wyliczania silni z przykładu 5 napisz
+ * program SilniaWhile, który używając instrukcji while() obliczy silnię z
+ * wprowadzonej liczby.
+ */
+public class SilniaWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę całkowitą dodatnią: ");
+        int liczba = scanner.nextInt();
+        long silnia = 1;
+        int i = 1;
+        while (i <= liczba) {
+            silnia *= i;
+            i ++;
+        }
+        System.out.printf("Silnia: %d! = %d", liczba, silnia);
+    }
 }
